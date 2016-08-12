c921cce96a977b23022ad19fcb4f700eda02b55f
Szél Péter <szelpeter@szelpeter.hu>
2016-08-12 12:06:45 +0200

    Updated build scripts

--- Build/createPackage.ps1
+++ Build/createPackage.ps1
@@ -1,8 +1,20 @@
-$env:Path=$env:Path + ";$env:ProgramFiles\MSBuild\14.0\bin"
-MSBuild.exe BarionClient.sln /t:Clean,Build /p:Configuration=Release
+param(
+	[bool]$rebuild = $true
+)
 
-Invoke-WebRequest -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile nuget.exe
+if($rebuild)
+{
+	$env:Path=$env:Path + ";$env:ProgramFiles\MSBuild\14.0\bin"
+	MSBuild.exe BarionClient.sln /t:Clean,Build /p:Configuration=Release
+}
 
-.\nuget.exe pack BarionClientLibrary\BarionClientLibrary.nuspec
+# Download nuget.exe if it's not in the PATH
+if ((Get-Command nuget -ErrorAction SilentlyContinue) -eq $null) 
+{
+	Invoke-WebRequest -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile nuget.exe
+	Set-Alias nuget .\nuget.exe
+}
 
-rm nuget.exe
+nuget pack BarionClientLibrary\BarionClientLibrary.nuspec
+
+rm .\nuget.exe -ErrorAction SilentlyContinue
--- Build/publishPackage.ps1
+++ Build/publishPackage.ps1
@@ -1,3 +1,12 @@
 $env:Path=$env:Path + ";$env:ProgramFiles\NuGet\"
 
+# Download nuget.exe if it's not in the PATH
+if ((Get-Command nuget -ErrorAction SilentlyContinue) -eq $null) 
+{
+	Invoke-WebRequest -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile nuget.exe
+	Set-Alias nuget .\nuget.exe
+}
+
 nuget.exe push .\BarionClient.*.nupkg -Source https://www.nuget.org/api/v2/package
+
+rm .\nuget.exe -ErrorAction SilentlyContinue
