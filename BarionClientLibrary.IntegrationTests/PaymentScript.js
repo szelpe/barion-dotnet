602ba8eaa21b38c3fe55607abe28687625ea6627
Lajos Marton <lajosmarton@hotmail.col>
2022-06-28 22:58:09 +0200

    PaymentScript used without jquery

--- BarionClientLibrary.IntegrationTests/PaymentScript.js
+++ BarionClientLibrary.IntegrationTests/PaymentScript.js
@@ -3,15 +3,15 @@ var page = require('webpage').create();
 
 page.open(system.args[1], function (status) {
     page.evaluate(function (loginName, password) {
-        $('[data-target="#PaymentMethodBarion"]').click();
-        $('#LoginName').val(loginName);
-        $('#Password').val(password);
-        $('button[type="submit"]').click();
+        document.querySelector('[data-target="#PaymentMethodBarion"]').click();
+        document.getElementById('LoginName').value = loginName;
+        document.getElementById('Password').value = password;
+        document.querySelector('button[type="submit"]').click();
     }, system.args[2], system.args[3]);
 
     setTimeout(function () {
         page.evaluate(function () {
-            $('button[type="submit"]').click();
+            document.getElementById('StartPayment').click();
         });
 
         setTimeout(function () {
